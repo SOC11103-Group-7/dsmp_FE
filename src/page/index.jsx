3d114d93526db805f0c72a70c6848034d5c1d9e4
htoomyattun <40516588@live.napier.ac.uk>
2024-02-23 19:15:21 +0000

    adjustments

--- src/page/index.jsx
+++ src/page/index.jsx
@@ -5,16 +5,13 @@ import ProfileIcon from '../assets/images/profile.png';
 import UserAvatar from '../assets/images/78919fd0-ee13-4419-80f3-1c857dd6aea4.png';
 import LogoImage from '../assets/images/96858bf39fb28499437875ab86a6d1dcd335a243.png';
 
-
-
 export default function Main() {
   return (
-    
     <div className='flex bg-yellow-50'>
-      <div className='w-64 border-r border-black overflow-hidden'>
+      <div className='w-64 h-lvh border-r border-black overflow-hidden'>
         <div className='m-4 '>
           <span className='flex items-center overflow-hidden'>
-          <div className='w-48 h-8 bg-cover bg-center bg-no-repeat' style={{ backgroundImage: `url(${LogoImage})` }}></div>
+            <div className='w-48 h-8 bg-cover bg-center bg-no-repeat' style={{ backgroundImage: `url(${LogoImage})` }}></div>
           </span>
           <div className='mt-6'>
             <span className='flex items-center text-base font-semibold text-gray-900 text-xl'>
@@ -31,31 +28,35 @@ export default function Main() {
         </div>
       </div>
       <div className='flex flex-col w-full md:w-1/2 px-10'>
-        <div className='w-full h-40 bg-gray-300 border border-black rounded-lg overflow-hidden mt-6'>
+        <div className='w-full h-40 bg-box border border-black rounded-lg overflow-hidden mt-6 bg-box'>
           <input type='text' className='w-full h-full p-4 font-bold text-sm text-gray-900 bg-transparent outline-none' placeholder="What's on your mind??" />
         </div>
-
         <div className='flex justify-end'>
-        <button className='w-20 h-8 mt-2 bg-zinc-700 text-white font-bold text-xs rounded-md'>Post</button>
+          <button className='w-20 h-8 mt-2 bg-zinc-700 text-white font-bold text-xs rounded-md'>Post</button>
         </div>
-
-        <div className='w-full bg-gray-300 border border-black rounded-lg overflow-hidden mt-4'>
-          <div className='flex items-center p-2'>
-            <div className='w-10 h-10 bg-cover bg-center bg-no-repeat rounded-full' style={{ backgroundImage: `url(${UserAvatar})` }}></div>
-            <div className='ml-2'>
-              <span className='font-bold text-sm text-gray-900'>John Doe</span>
-              <span className='text-xs text-red-600 ml-2'>25mins ago</span>
+        <div className='w-full bg-box border border-black rounded-lg overflow-hidden mt-4 '>
+          <div className='ml-2 mt-2'>
+            <div className='flex items-center'>
+              <div className='w-10 h-10 bg-cover bg-center bg-no-repeat rounded-full' style={{ backgroundImage: `url(${UserAvatar})` }}> </div>
+              <div className='ml-2'>
+                <span className='font-bold text-sm text-gray-900'>John Doe</span>
+                <span className='text-xs text-red-600 ml-2'>25 minutes ago</span>
+                
+              </div>
+              
+            </div>
+            <div className='ml-10'>
+            <p className='p-2 text-sm text-gray-900 text-justify'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
             </div>
           </div>
-          <p className='p-2 text-sm text-gray-900'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-
-          <span className='flex justify-end'>
-          <span className='block p-2 text-sm font-bold text-gray-900'>0.0ETH</span>
-          <button className='w-16 h-6 m-2 bg-gray-300 text-gray-900 font-bold text-xs rounded-md border border-black'>Tip 0.1ETH</button>
-          </span>
-          
+          <div className='flex justify-end'>
+           <div className="flex flex-col  place-items-center ">
+               <span className=' block p-2 text-sm font-bold text-gray-900'>0.0ETH</span>
+              <button className='w-20 h-8 m-2 bg-gray-300 text-gray-900 font-semibold text-xs rounded-full border border-black bg-yellow-50'>Tip 0.1ETH</button>
+          </div>
+          </div>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
+  
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
+      colors: {
+        'box':'#D8D9C9',
+      }
       
     },
+    
   },
   variants: {
     extend: {},
